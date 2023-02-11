0645bcd5b632e3e85c12ff0d76e7b88d4e5c3a68
webashraf <aliashraftamim@gmail.com>
2023-02-12 00:18:56 +0600

    Added baper bank project

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById('submitButton').addEventListener('click', function(){
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    if (email == 'aliashraftamim@gmail.com' && password == '10612619tam') {
+        console.log('Valid User');
+        window.location.href = 'bank.html'
+    }
+    else{
+        alert('Wrong Email or Password')
+    }
+    // console.log(email, password);
+})
