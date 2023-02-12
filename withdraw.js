1949ffeb063ebbd2781e538866dfcbc61d9c1d89
webashraf <aliashraftamim@gmail.com>
2023-02-12 18:06:40 +0600

    add baper bank codes

--- withdraw.js
+++ withdraw.js
@@ -7,12 +7,19 @@ document.getElementById('withdraw-btn').addEventListener('click', function(){
     const withdrawUpvalueToNumber = parseFloat(withdrawUpValue);
     console.log(withdrawUpvalueToNumber);
     const totalWithdraw = withDrawValue + withdrawUpvalueToNumber;
-    document.getElementById('withdraw-up-value').innerText = totalWithdraw
     console.log(withdrawUpValue);
     let upBalance = document.getElementById('balnce-up-value');
+    document.getElementById('withdraw-field').value = '';
+
+    if (totalWithdraw > upBalance.innerText) {
+        alert('Baper bank a eto taka nai')
+        return;
+    }
+    document.getElementById('withdraw-up-value').innerText = totalWithdraw;
+
+    // ! Blance Update
     const upBalanceToNumber = parseFloat(upBalance.innerText);
     const totalBalance = upBalanceToNumber - totalWithdraw;
     document.getElementById('balnce-up-value').innerText = totalBalance;
-    document.getElementById('withdraw-field').value = '';
-    console.log(document.getElementById('withdraw-up-value').innerText);
+    // console.log(document.getElementById('withdraw-up-value').innerText);
 })
